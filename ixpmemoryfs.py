8f3404cf66954bba9917fecc8f233d5bb602036c
Joël Franusic <joel@franusic.com>
2024-08-10 21:34:25 -0700

    I can read and write to things on the base path

---
+++ ixpmemoryfs.py
@@ -0,0 +1,454 @@
+from __future__ import annotations
+
+import logging
+from datetime import datetime, timezone
+from errno import ENOTEMPTY
+from io import BytesIO
+from pathlib import PurePath, PureWindowsPath
+from typing import Any, ClassVar
+import os
+import hashlib
+
+from fsspec import AbstractFileSystem
+from fsspec.implementations.local import LocalFileSystem
+from fsspec.utils import stringify_path
+
+import ixpy
+
+logger = logging.getLogger("fsspec.ixpmemoryfs")
+
+class IxpMemoryFileSystem(AbstractFileSystem):
+    """A filesystem based on a dict of BytesIO objects
+
+    This is a global filesystem so instances of this class all point to the same
+    in memory filesystem.
+    """
+
+    protocol = "ixpmemory"
+    root_marker = "/"
+
+    def __init__(self, *args, **kwargs):
+        self.store = IxpMemoryFile(path="/")
+        self.store.qid._type.directory = True
+        self.store.qid._path = 0
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def _strip_protocol(cls, path):
+        if isinstance(path, PurePath):
+            if isinstance(path, PureWindowsPath):
+                return LocalFileSystem._strip_protocol(path)
+            else:
+                path = stringify_path(path)
+
+        protocol = "ixpmemory"
+        if path.startswith(f"{protocol}://"):
+            path = path[len(f"{protocol}://") :]
+        if "::" in path or "://" in path:
+            return path.rstrip("/")
+        path = path.lstrip("/").rstrip("/")
+        return "/" + path if path else ""
+
+    def ls(self, path, detail=True, **kwargs):
+        path = self._strip_protocol(path)
+        try:
+            items = self.store.get_by_path(path)._children
+        except Exception as e:
+            raise FileNotFoundError(f"{path} - {str(e)}")
+        out = []
+        as_9p = kwargs.get("as_9p", False)
+        for item in items:
+            stat = item.fs_stat()
+            if as_9p:
+                stat = item.as_9p()
+            out.append(stat)
+        if detail:
+            return out
+        return sorted([f["name"] for f in out])
+
+    def mkfunc(self, path, obj):
+        path = self._strip_protocol(path)
+        if not self.isdir(path):
+            raise NotADirectoryError(f"mkfunc path={path} MUST be a directory")
+        directory = self.store.get_by_path(path)
+        directory.add_child(obj)
+
+    def mkdir(self, path, create_parents=True, **kwargs):
+        path = self._strip_protocol(path)
+        item = self.store.get_by_path(path)
+
+        if item and self.isfile(path):
+            raise FileExistsError(path)
+        if self._parent(path).strip("/") and self.isfile(self._parent(path)):
+            raise NotADirectoryError(self._parent(path))
+        if create_parents and self._parent(path).strip("/"):
+            try:
+                self.mkdir(self._parent(path), create_parents, **kwargs)
+            except FileExistsError:
+                pass
+
+        parent = self.store.get_by_path(self._parent(path))
+        if parent and not item:
+            child = IxpMemoryFile(path=path)
+            child.qid._type.directory = True
+            parent.add_child(child)
+
+
+    def makedirs(self, path, exist_ok=False):
+        try:
+            self.mkdir(path, create_parents=True)
+        except FileExistsError:
+            if not exist_ok:
+                raise
+
+    def pipe_file(self, path, value, **kwargs):
+        """Set the bytes of given file
+
+        Avoids copies of the data if possible
+        """
+        self.open(path, "wb", data=value)
+
+    def rmdir(self, path):
+        path = self._strip_protocol(path)
+        parent_path = self._parent(path)
+        child_name = os.path.basename(path)
+        # print(f"rmdir(path={path}) parent={parent_path} child={child_name}")
+
+        child = self.store.get_by_path(path)
+        parent = self.store.get_by_path(parent_path)
+        if path == "":
+            # silently avoid deleting FS root
+            return
+        if child:
+            if not self.ls(path):
+                parent.remove_child_by_name(child_name)
+            else:
+                raise OSError(ENOTEMPTY, "Directory not empty", path)
+        else:
+            raise FileNotFoundError(path)
+
+    def info(self, path, **kwargs):
+        logger.debug("info: %s", path)
+        path = self._strip_protocol(path)
+        item = self.store.get_by_path(path)
+        if not item:
+            raise FileNotFoundError(path)
+        elif kwargs.get("as_9p", False):
+            return item.as_9p()
+        else:
+            return item.fs_stat()
+
+    def chmod(self, path, mode):
+        logger.debug("chmod: %s", path)
+        path = self._strip_protocol(path)
+        item = self.store.get_by_path(path)
+        if not item:
+            raise FileNotFoundError(path)
+        item.mode = mode
+        return True
+
+    def chown(self, path, user_id, group_id):
+        logger.debug("chmod: %s", path)
+        path = self._strip_protocol(path)
+        item = self.store.get_by_path(path)
+        if not item:
+            raise FileNotFoundError(path)
+        if user_id:
+            item.uid = user_id
+        if group_id:
+            item.gid = group_id
+        return True
+
+
+    def utime(self, path, times=None):
+        logger.debug(f"utime({path}, {times})")
+        path = self._strip_protocol(path)
+        item = self.store.get_by_path(path)
+        if not item:
+            raise FileNotFoundError(path)
+        atime = times[0]
+        mtime = times[1]
+        logger.debug(f"utime: atime={atime} mtime={mtime})")
+        if atime:
+            item.atime = atime
+        if mtime:
+            item.mtime = mtime
+        return True
+
+
+    def _raw(self, path, **kwargs):
+        return self.info(path, as_9p=True, **kwargs)
+
+    def _open(
+        self,
+        path,
+        mode="rb",
+        block_size=None,
+        autocommit=True,
+        cache_options=None,
+        **kwargs,
+    ):
+        path = self._strip_protocol(path)
+        item = self.store.get_by_path(path)
+
+        if item and item.qid._type.directory:
+            raise IsADirectoryError(path)
+        parent = path
+        # This checks to see if any part of the path is a file
+        while len(parent) > 1:
+            parent = self._parent(parent)
+            if self.isfile(parent):
+                raise FileExistsError(parent)
+        if mode in ["rb", "ab", "r+b"]:
+            if item:
+                f = item
+                if mode == "ab":
+                    # position at the end of file
+                    f.seek(0, 2)
+                else:
+                    # position at the beginning of file
+                    f.seek(0)
+                return f
+            else:
+                raise FileNotFoundError(path)
+        elif mode == "wb":
+            m = IxpMemoryFile(fs=self, path=path, data=kwargs.get("data"))
+            if not self._intrans:
+                m.commit()
+            parent_dir = self.store.get_by_path(self._parent(path))
+            parent_dir.add_child(m)
+            return m
+        else:
+            name = self.__class__.__name__
+            raise ValueError(f"unsupported file mode for {name}: {mode!r}")
+
+    def cp_file(self, path1, path2, **kwargs):
+        path1 = self._strip_protocol(path1)
+        path2 = self._strip_protocol(path2)
+        path2_name = os.path.basename(path2)
+        if self.isfile(path1):
+            path1_file = self.store.get_by_path(path1)
+            path2_parent = self.store.get_by_path(self._parent(path2))
+            path2_file = self.store[path2] = IxpMemoryFile(
+                fs=self, path=path2, data=path1_file.getvalue()
+            )  # implicit copy
+            path2_parent.add_child(path2_file)
+        elif self.isdir(path1):
+            # FIXME: Is this even what I want?
+            if not path2_parent.contains_child(path2_name):
+                path1_file = self.store.get_by_path(path1)
+                path1_copy = copy.deepcopy(path1_file)
+                path2_parent.add_child(path1_copy)
+        else:
+            raise FileNotFoundError(path1)
+
+    def cat_file(self, path, start=None, end=None, **kwargs):
+        logger.debug("cat: %s", path)
+        path = self._strip_protocol(path)
+        f = self.store.get_by_path(path)
+        try:
+            return bytes(f.getbuffer()[start:end])
+        except KeyError:
+            raise FileNotFoundError(path)
+
+    def _rm(self, path):
+        path = self._strip_protocol(path)
+        name = os.path.basename(path)
+        parent = self.store.get_by_path(self._parent(path))
+
+        if parent.contains_child(name):
+            parent.remove_child_by_name(name)
+        else:
+            raise FileNotFoundError(path)
+
+    def modified(self, path):
+        info = self.info(path)
+        return info["mtime"]
+
+    def created(self, path):
+        info = self.info(path)
+        return info["mtime"]
+
+    def rm(self, path, recursive=False, maxdepth=None):
+        if isinstance(path, str):
+            path = self._strip_protocol(path)
+        else:
+            path = [self._strip_protocol(p) for p in path]
+        # FIXME: This doesn't work
+        paths = self.expand_path(path, recursive=recursive, maxdepth=maxdepth)
+        print(f"rm paths {paths}")
+        for p in reversed(paths):
+            # If the expanded path doesn't exist, it is only because the expanded
+            # path was a directory that does not exist in self.pseudo_dirs. This
+            # is possible if you directly create files without making the
+            # directories first.
+            if not self.exists(p):
+                continue
+            if self.isfile(p):
+                self.rm_file(p)
+            else:
+                self.rmdir(p)
+
+class QIDType():
+    def __init__(self):
+        self.directory = False
+        self.append = False
+        self.exclusive = False
+        self.mount = False
+        self.auth_file = False
+        self.temp_file = False
+
+class QID():
+    def __init__(self, path):
+        self._type = QIDType()
+        self._version = 1
+        self._path = self._to_path_number(path)
+
+    def _to_path_number(self, path):
+        # Create SHA-1 hash of the string
+        sha1_hash = hashlib.sha1(path.encode()).digest()
+
+        # Take the first N bytes (8 bits per byte) of the hash
+        first_n_bytes = sha1_hash[:8]
+        result = int.from_bytes(first_n_bytes, byteorder='big', signed=False)
+        return result
+
+
+
+class IxpMemoryFile(BytesIO):
+    """A BytesIO which can't close and works as a context manager
+
+    Can initialise with data. Each path should only be active once at any moment.
+
+    No need to provide fs, path if auto-committing (default)
+    """
+
+    def __init__(self, fs=None, path=None, data=None, mode=0o755, uid="glenda", gid="glenda"):
+        logger.debug("open file %s", path)
+        self.fs = fs
+        name = os.path.basename(path)
+
+        self.qid = QID(path=path)
+        self._mode = mode
+        self.atime = datetime.now(tz=timezone.utc)
+        self.mtime = datetime.now(tz=timezone.utc)
+        self.name = name
+        self.uid = uid
+        self.gid = gid
+        self.muid = uid
+        self._path = path
+        self._children = []
+        if data:
+            super().__init__(data)
+            self.seek(0)
+
+    @property
+    def length(self):
+        return self.getbuffer().nbytes
+
+    @property
+    def mode(self):
+        mode = self._mode
+        if self.qid._type.directory:
+            mode = mode | ixpy.DMDIR
+        return mode
+
+    @mode.setter
+    def mode(self, value):
+        self._mode = value
+
+    def __enter__(self):
+        return self
+
+    def close(self):
+        pass
+
+    def discard(self):
+        pass
+
+    def commit(self):
+        # self.fs.store[self.path] = self
+        self.modified = datetime.now(tz=timezone.utc)
+
+    def as_9p(self):
+        return {
+            "qid": {
+                "type": {
+                    "dir": self.qid._type.directory,
+                    "append": self.qid._type.append,
+                    "exclusive": self.qid._type.exclusive,
+                    "mount": self.qid._type.mount,
+                    "auth_file": self.qid._type.auth_file,
+                    "temp_file": self.qid._type.temp_file,
+                },
+                "version": self.qid._version,
+                "path": self.qid._path,
+            },
+            "mode": self.mode,
+            "atime": self.atime.isoformat().replace("+00:00", "Z"),
+            "mtime": self.mtime.isoformat().replace("+00:00", "Z"),
+            "length": self.length,
+            "name": self.name,
+            "uid": self.uid,
+            "gid": self.gid,
+            "muid": self.muid,
+        }
+
+    def fs_stat(self):
+        return {
+            "name": self.name,
+            "type": "directory" if self.qid._type.directory else "file",
+            "size": self.length,
+
+            "qid": self.qid,
+            "mode": self.mode,
+            "accessed": self.atime.timestamp(),
+            "modified": self.mtime.timestamp(),
+            "uid": self.uid,
+            "gid": self.gid,
+            "muid": self.muid,
+        }
+
+    def add_child(self, child):
+        self._children.append(child)
+
+    def contains_child(self, name):
+        for child in self._children:
+            if child.name == name:
+                return True
+        return False
+
+    def remove_child_by_name(self, name):
+        for i, child in enumerate(self._children):
+            if child.name == name:
+                del self._children[i]
+                return True  # Return True if the child was found and removed
+        return False  # Return False if no child with the specified name was found
+
+    def get_by_path(self, path):
+        # If the path is "/", return the current object (assuming it's the root)
+        if path == "/" or path == "":
+            return self
+
+        # Split the path into parts
+        parts = path.strip('/').split('/')
+
+        # Start with the current object
+        current = self
+
+        for part in parts:
+            # Search among the children for the matching name
+            found = None
+            for child in current._children:
+                if child.name == part:
+                    found = child
+                    break
+
+            if found is None:
+                # If no child with the specified name is found, return None or raise an error
+                return None
+
+            # Move to the found child
+            current = found
+
+        return current
---
+++ serve.py
@@ -0,0 +1,317 @@
+from datetime import datetime
+import hashlib
+import logging
+import os
+import socket
+import sys
+import threading
+import time
+import traceback
+
+import fsspec
+
+import ixpy
+from ixpy import IxpMessage, container_to_json, Builder
+from ixpmemoryfs import IxpMemoryFileSystem, IxpMemoryFile
+
+# Lesson learned: Don't use the fsspec memory filesystem, write my own that stores QID and path information natively
+
+class Server:
+    def __init__(self, host='localhost', port=8888):
+        self.host = host
+        self.port = port
+        self.socket = None
+        self.ixp = Builder()
+        self.ixp.log.setLevel(logging.DEBUG)
+        self.message = IxpMessage
+        self.log = logging.getLogger(self.__class__.__name__)
+        self.log.setLevel(logging.DEBUG)
+
+        self.fs = IxpMemoryFileSystem()
+        self.fs.mkdir("/usr/glenda/test/", parents=True)
+        # self.fs.mkdir("/usr/glenda/test2/", parents=True)
+        self.fs.mkdir("/dynamic", parents=True)
+        self.fs.touch("/dynamic/doop")
+        # with self.fs.open("/dynamic/hello", "r+b") as f:
+        #     f.seek(0)
+        #     f.write(b"Hello world?")
+
+        self.fid = {
+            0: "/"
+        }
+
+
+
+    def start(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind((self.host, self.port))
+        self.socket.listen(5)
+        self.log.info(f"Server started on {self.host}:{self.port}")
+
+        while True:
+            client_socket, addr = self.socket.accept()
+            self.log.info(f"Connection from {addr}")
+            try:
+                self.handle_client(client_socket)
+            except Exception as e:
+                self.log.error(f"Error handling client: {str(e)}")
+                traceback.print_exception(type(e), e, e.__traceback__)
+            finally:
+                client_socket.close()
+
+    def stop(self):
+        pass
+
+    def handle_client(self, client_socket):
+        while True:
+            message = client_socket.recv(self.ixp.msize)
+            request = self.message.parse(message)
+            self.log.info(f"Received: {container_to_json(request)}")
+
+            try:
+                response = self.handle_message(request)
+            except Exception as e:
+                response = self.ixp.Terror(fid=request.fid, ename=f"General error: {e}")
+            client_socket.sendall(self.message.build(response))
+
+    def paths_from_wnames(self, wnames):
+        paths = []
+        for i in range(1, len(wnames) + 1):
+            path = '/' + '/'.join(wnames[:i])
+            paths.append(path)
+        return paths
+
+    def paths_from_path(self, path: str):
+        paths = []
+        wnames = path.strip("/").split("/")
+        for i in range(1, len(wnames) + 1):
+            path = '/' + '/'.join(wnames[:i])
+            paths.append(path)
+        return paths
+
+    def _path_to_qid(self, path):
+        self.log.debug(f"_path_to_qid({path})")
+        try:
+            info = self.fs.info(path, as_9p=True)
+        except FileNotFoundError:
+            self.log.debug(f"_path_to_qid: path not found {path}")
+            return None
+        except Exception as e:
+            self.log.debug(f"_path_to_qid({path}) got unhandled exception: {type(e).__name__}: {e}")
+            return None
+        self.log.debug(f"_path_to_qid: got info: {info}")
+        return info["qid"]
+
+
+    def _handle_walk(self, request):
+        # self.log.debug(f"_handle_walk: wnames={request.payload.wnames}")
+        wnames = request.payload.wnames
+        for wname in wnames:
+            if "/" in wname:
+                return self.ixp.Rerror(tag=request.tag, ename=f"wname '{wname}' must not contain '/'")
+
+        current_path = self.fid.get(request.payload.fid, "/")
+        print(f"_handle_walk: current_path={current_path} wnames={wnames}")
+        full_path = os.path.join(current_path, *wnames)
+        print(f"_handle_walk: full_path={full_path}")
+
+        # paths = self.paths_from_path(full_path)
+        paths = self.paths_from_wnames(wnames)
+        self.log.debug(f"_handle_walk: paths={paths}")
+        wqids = []
+        for path in paths:
+            try:
+                qid = self._path_to_qid(path)
+            except Exception as e:
+                return self.ixp.Rerror(tag=request.tag, ename=f"{e}")
+            if qid:
+                wqids.append(qid)
+        self.log.debug(f"_handle_walk SET FID {request.payload.newfid} = {full_path} which is from {wnames}")
+        self.fid[request.payload.newfid] = full_path
+        return self.ixp.Rwalk(tag=request.tag, wqids=wqids)
+
+    def _listdir(self, path):
+        self.log.debug(f"_listdir({path})")
+        results = self.fs.ls(path, detail=True, as_9p=True)
+        self.log.debug(f"_listdir results: '{results}'")
+        data = ixpy.StatPayloads.build(results)
+        return data
+
+    def handle_message(self, request):
+        if request.type == 'Tversion':
+            rv =  self.ixp.Rversion(tag=request.tag,
+                                    msize=request.payload.msize,
+                                    version="9P2000")
+            return rv
+        elif request.type == 'Tattach':
+            return self.ixp.Rattach(
+                tag=request.tag,
+                qid={
+                    "type": {
+                        "dir": True,
+                        "append": False,
+                        "exclusive": False,
+                        "mount": False,
+                        "auth_file": False,
+                        "temp_file": False,
+                    },
+                    "version": 0,
+                    "path": 0,
+                }
+            )
+        elif request.type == 'Twalk':
+            return self._handle_walk(request)
+        elif request.type == 'Tcreate':
+            fid = request.payload.fid
+            perm = request.payload.perm
+            is_dir = (perm & ixpy.DMDIR) != 0
+            base = self.fid[fid]
+            path = os.path.join(base, request.payload.name)
+            print(f"Tcreate on {fid} which is {base} = {path}")
+            if is_dir:
+                self.fs.mkdir(path)
+            else:
+                self.fs.touch(path)
+            qid = self.fs.info(path, as_9p=True)["qid"]
+            return self.ixp.Rcreate(tag=request.tag, qid=qid, iounit=self.ixp.msize)
+        elif request.type == "Tstat":
+            fid = request.payload.fid
+            path = self.fid[fid]
+            try:
+                stat_payload = self.fs.info(path, as_9p=True)
+            except Exception as e:
+                return self.ixp.Rerror(tag=request.tag, ename=f"Tstat error: {e}")
+            return self.ixp.Rstat(tag=request.tag, **stat_payload)
+        elif request.type == "Topen":
+            fid = request.payload.fid
+            path = self.fid[fid]
+            qid = self.fs.info(path, as_9p=True)["qid"]
+            if not qid:
+                return self.ixp.Rerror(tag=request.tag, ename=f"Error getting QID for FID={fid}")
+            iounit = self.ixp.msize
+            if path == "/":
+                iounit = 0
+            return self.ixp.Ropen(tag=request.tag, qid=qid, iounit=iounit)
+        elif request.type == "Twrite":
+            fid = request.payload.fid
+            offset = request.payload.offset
+            count = request.payload.count
+            data = request.payload.data
+            path = self.fid[fid]
+            written = 0
+            # self.log.debug(f"Twrite path={path} offset={offset} count={count} data={data}")
+            # FIXME: "r+b" should be dynamic!!!!
+            with self.fs.open(path, "r+b") as f:
+                f.seek(offset)
+                written += f.write(data[:count])
+            return self.ixp.Rwrite(tag=request.tag, count=written)
+        elif request.type == "Tread":
+            fid = request.payload.fid
+            path = self.fid[fid]
+            self.log.debug(f"Tread fid={fid} path={path} open fids: {self.fid}")
+            if self.fs.isdir(path):
+                self.log.debug("Tread is dir")
+                data = self._listdir(path)
+                self.log.debug(f"Tread len(data)={len(data)}")
+                if request.payload.offset >= len(data):
+                    data = b""
+            else:
+                self.log.debug("Tread NOT dir")
+                offset = request.payload.offset
+                count = request.payload.count
+                start = offset
+                end = start + count
+                data = self.fs.cat_file(path, start=start, end=end)
+            return self.ixp.Rread(tag=request.tag, data=data, count=len(data))
+        elif request.type == "Tremove":
+            fid = request.payload.fid
+            path = self.fid[fid]
+            self.fs.rm(path)
+            return self.ixp.Rremove(tag=request.tag)
+        elif request.type == "Twstat":
+            fid = request.payload.fid
+            path = self.fid[fid]
+
+            mode = request.payload.stat.mode
+            if mode != ixpy.MODE_MAX:
+                print(f"Twstat chmod({path}, {mode})")
+                self.fs.chmod(path, mode)
+
+            user_id = None
+            if request.payload.stat.uid:
+                user_id = request.payload.stat.uid
+            group_id = None
+            if request.payload.stat.gid:
+                group_id = request.payload.stat.gid
+
+            if user_id or group_id:
+                print(f"Twstat chown({path}, {user_id}, {group_id})")
+                self.fs.chown(path, user_id, group_id)
+
+            TIME_MAX = "1969-12-31T23:59:59.000000000Z"
+
+            atime = None
+            if request.payload.stat.atime != TIME_MAX:
+                atime = datetime.fromisoformat(request.payload.stat.atime)
+            mtime = None
+            if request.payload.stat.mtime != TIME_MAX:
+                mtime = datetime.fromisoformat(request.payload.stat.mtime)
+            if atime or mtime:
+                self.fs.utime(path, (atime, mtime))
+
+            return self.ixp.Rwstat(tag=request.tag)
+        elif request.type == "Tclunk":
+            del self.fid[request.payload.fid]
+            return self.ixp.Rclunk(tag=request.tag)
+        else:
+            return self.ixp.Rerror(tag=request.tag, ename=f"Unknown request type: {request.type}")
+
+class DynamicFile(IxpMemoryFile):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def read(self, size):
+        return b"Hello world!"
+
+    # def write(self, value):
+    #     pass
+
+    # def getvalue(self):
+    #     pass
+
+    # def seek(self, offset, whence=0):
+    #     pass
+
+    # def close(self):
+    #     pass
+
+def start_server():
+    try:
+        server = Server()
+        # m = IxpMemoryFile(fs=server.fs, path="/dynamic/fnordzz")
+        # m.qid._type.directory = True
+        # server.fs.mkfunc("/dynamic", m)
+        # server.fs.touch("/dynamic/touched")
+        server.start()
+    except KeyboardInterrupt:
+        print("Server shutting down")
+    finally:
+        server.stop()
+
+def monitor_file_changes(server_thread, file_path):
+    last_mtime = os.path.getmtime(file_path)
+
+    while server_thread.is_alive():
+        current_mtime = os.path.getmtime(file_path)
+        if current_mtime != last_mtime:
+            print("File changed, restarting server...")
+            os.execv(sys.executable, ['python'] + sys.argv)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    server_thread = threading.Thread(target=start_server)
+    server_thread.start()
+
+    # Monitor changes to the source file
+    monitor_file_changes(server_thread, __file__)
